0a8b054ed828df8514ed157bae1c8d4176e03901
Emerson Mendes <devmendesc@gmail.com>
2024-08-28 15:46:59 -0300

    chore: adicionar bibliotecas Retrofit, Koin, Navigation e outras
    - Adicionada a biblioteca Koin para injeção de dependências (versão 3.5.6)
    - Adicionada a biblioteca Retrofit para requisições HTTP (versão 2.9.0)
    - Adicionado conversor de Kotlinx Serialization para Retrofit (versão 0.8.0)
    - Adicionada a biblioteca Navigation Compose para navegação (versão 2.7.3)
    - Adicionada a extensão de ícones Material 3 (versão 1.2.0)
    - Adicionadas as bibliotecas Kotlinx Serialization para JSON (versão 1.6.0) e XML (versão 0.5.0)
    - Adicionada a biblioteca Kotlin Serialization (versão 1.9.10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+
 }
 
 android {
@@ -66,4 +68,21 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Koin
+    implementation(libs.koin.androidx.compose)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Kotlinx Serialization (XML)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.serialization.xml)
+
+    // Kotlin Serializable
+    implementation(libs.kotlin.serialization)
 }
