1f340ce50ae3268ecc26dfac8171394871e01fc7
Emerson Mendes <devmendesc@gmail.com>
2024-09-02 22:00:14 -0300

    feat: Remoção da inicialização do koin da main

--- app/src/main/java/com/br/devmendesc/playcast/MainActivity.kt
+++ app/src/main/java/com/br/devmendesc/playcast/MainActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.br.devmendesc.Navigation
+import androidx.navigation.compose.rememberNavController
+import com.br.devmendesc.playcast.navigation.SetupNavigation
 import com.br.devmendesc.playcast.di.playCastModule
 import com.br.devmendesc.playcast.ui.theme.PlayCastTheme
 import org.koin.android.ext.koin.androidContext
@@ -14,15 +15,10 @@ import org.koin.core.context.startKoin
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startKoin {
-            androidLogger()
-            androidContext(this@MainActivity)
-            modules(playCastModule)
-        }
         enableEdgeToEdge()
         setContent {
             PlayCastTheme {
-                Navigation()
+                SetupNavigation(navController = rememberNavController())
             }
         }
     }
