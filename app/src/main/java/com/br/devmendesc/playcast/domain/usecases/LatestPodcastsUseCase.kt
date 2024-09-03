f6277c5f88b18e952f212fba967410bba5c5792a
Emerson Mendes <devmendesc@gmail.com>
2024-09-03 18:38:40 -0300

    feat: Alteração nos casos de uso, inclusão do repositorio, e ajustes nas chamadas

--- app/src/main/java/com/br/devmendesc/playcast/domain/usecases/LatestPodcastsUseCase.kt
+++ app/src/main/java/com/br/devmendesc/playcast/domain/usecases/LatestPodcastsUseCase.kt
@@ -1,61 +1,19 @@
 package com.br.devmendesc.playcast.domain.usecases
 
-import com.br.devmendesc.playcast.domain.vo.PodcastVO
+import com.br.devmendesc.playcast.data.repository.PodcastRepository
 import com.br.devmendesc.playcast.ui.view.home.HomePageUiState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-open class LatestPodcastsUseCase() {
-    private val podcasts = listOf(
-        PodcastVO(
-            title = "Tech Talks Daily",
-            image = "https://t3.ftcdn.net/jpg/05/17/08/90/360_F_517089048_ZXTKBelIKt7AoeJIJ9ftoOpnKEz2KHPc.jpg",
-            episodes = listOf(),
-            description = "",
-            authors = ""
-        ),
-        PodcastVO(
-            title = "History Extra",
-            image = "https://lets.events/blog/wp-content/uploads/2022/05/podcast-online-scaled.jpg",
-            episodes = listOf(),
-            description = "",
-            authors = ""
-        ),
-        PodcastVO(
-            title = "Science Vs",
-            image = "https://lets.events/blog/wp-content/uploads/2022/05/podcast-online-scaled.jpg",
-            episodes = listOf(),
-            description = "",
-            authors = ""
-        ),
-        PodcastVO(
-            title = "The Daily",
-            image = "https://t3.ftcdn.net/jpg/05/17/08/90/360_F_517089048_ZXTKBelIKt7AoeJIJ9ftoOpnKEz2KHPc.jpg",
-            episodes = listOf(),
-            description = "",
-            authors = ""
-        ),
-        PodcastVO(
-            title = "Stuff You Should Know",
-            image = "https://lets.events/blog/wp-content/uploads/2022/05/podcast-online-scaled.jpg",
-            episodes = listOf(),
-            description = "",
-            authors = ""
-        ),
-        PodcastVO(
-            title = "TED Talks Daily",
-            image = "https://t3.ftcdn.net/jpg/05/17/08/90/360_F_517089048_ZXTKBelIKt7AoeJIJ9ftoOpnKEz2KHPc.jpg",
-            episodes = listOf(),
-            description = "",
-            authors = ""
-        )
-    )
+open class LatestPodcastsUseCase(
+    private val repository: PodcastRepository
+) {
     operator fun invoke(): Flow<HomePageUiState> = flow {
         runCatching {
-            podcasts
+            repository.getLastSearchPodcast()
         }
             .onSuccess { response ->
-                emit(HomePageUiState.LatestPodcastLoaded(response))
+                emit(HomePageUiState.LatestPodcastLoaded(response ?: listOf()))
             }
             .onFailure {
                 emit(HomePageUiState.PodcastError)
--- app/src/main/java/com/br/devmendesc/playcast/domain/usecases/LoadingPodcastUseCase.kt
+++ app/src/main/java/com/br/devmendesc/playcast/domain/usecases/LoadingPodcastUseCase.kt
@@ -2,27 +2,26 @@ package com.br.devmendesc.playcast.domain.usecases
 
 import android.util.Log
 import com.br.devmendesc.playcast.data.repository.PodcastRepository
-import com.br.devmendesc.playcast.domain.mapper.PodcastMapper
 import com.br.devmendesc.playcast.ui.view.home.HomePageUiState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 open class LoadingPodcastUseCase(
     private val podcastRepository: PodcastRepository,
-    private val podcastMapper: PodcastMapper
-
 ) {
     operator fun invoke(url: String): Flow<HomePageUiState> = flow {
         runCatching {
-           podcastRepository.fetchPodcast(url)
+            podcastRepository.fetchPodcast(url)
+
         }
-            .onSuccess { podcastFeed ->
-                Log.d("Response", "Response: ${podcastFeed.channel?.items?.get(1)}")
-                val podcastVO = podcastMapper.mapToPodcastVO(podcastFeed)
-                emit(HomePageUiState.PodcastLoaded(podcastVO))
+            .onSuccess { podcast ->
+                val podcastWithLink = podcast.apply {
+                    link = url
+                }
+                podcastRepository.savePodcast(podcastWithLink)
+                emit(HomePageUiState.PodcastLoaded(podcastWithLink))
             }
-            .onFailure {throwable ->
-                Log.e("LoadingPodcastUseCase", "Failed to load podcast", throwable)
+            .onFailure {
                 emit(HomePageUiState.PodcastError)
             }
     }
